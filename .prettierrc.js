9602bc54ceaff517b962c2c47f4e73ebf145f94b
April <april.mulvey@gmail.com>
2021-11-02 13:13:43 -0700

    Adding more stuff for conversion

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    trailingComma: "es5",
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    bracketSpacing: true,
+    jsxBrackets: true,
+    printWidth: 80,
+    proseWrap: "preserve",
+    quoteProps: "as-needed"
+  }
