922813fc97039fc2d1c874d222be3bac72b23177
Jeff Maciejko <design@jeffmaciejko.com>
2024-05-27 17:24:27 -0700

    Add base error boundary to starter

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { QueryClientProvider } from '@tanstack/react-query'
+import ErrorBoundary from 'components/error-boundary'
 import { Layouts } from 'components/layouts'
 import { queryClient } from 'util/query-client'
 
@@ -6,11 +7,13 @@ import 'styles/index.scss'
 
 function App({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Layouts layouts={Component.Layouts || []} pageProps={pageProps}>
-        <Component {...pageProps} />
-      </Layouts>
-    </QueryClientProvider>
+    <ErrorBoundary>
+      <QueryClientProvider client={queryClient}>
+        <Layouts layouts={Component.Layouts || []} pageProps={pageProps}>
+          <Component {...pageProps} />
+        </Layouts>
+      </QueryClientProvider>
+    </ErrorBoundary>
   )
 }
 
