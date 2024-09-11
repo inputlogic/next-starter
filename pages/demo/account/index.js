68fa9242a0bf59a83ef4bdb22af1ffa760338a41
Adriaan Mulder <adriaan.mulder@gmail.com>
2024-09-11 14:27:11 -0700

    Add my profile query example

--- pages/demo/account/index.js
+++ pages/demo/account/index.js
@@ -1,8 +1,14 @@
 import { LogoutForm } from 'components/forms/logout'
+import { useQuery } from 'hooks/use-query'
 
 export const Account = () => {
+  const query = useQuery({url: '/user/my-profile'})
   return <div>
     <h3>Account</h3>
+    {query.isLoading && <div>Loading...</div>}
+    {query.isSuccess && <div>
+      You are logged in as {query.data.email}
+    </div>}
     <LogoutForm submitButton={{variation: 'text'}} onSuccess={() => window.location.href = '/demo' } />
   </div>
 }
