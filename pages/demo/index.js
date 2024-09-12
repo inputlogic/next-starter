31948b909d3283ea0eaa47de3891631ee234e04e
Adriaan Mulder <adriaan.mulder@gmail.com>
2024-09-12 09:42:24 -0700

    Add minimal sentry setup

--- pages/demo/index.js
+++ pages/demo/index.js
@@ -1,10 +1,13 @@
 import Link from 'next/link'
 
+import styles from './demo.module.scss'
+
 export const DemoPage = () => {
   return <div>
     <h2>Demo</h2>
-    <div>
+    <div className={styles.links} >
       <Link href='/demo/modals'>Modals</Link>
+      <Link href='/demo/sentry'>Sentry Error Handling</Link>
     </div>
   </div>
 }
