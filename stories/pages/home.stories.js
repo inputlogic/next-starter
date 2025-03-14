e4cffe8fedf063a73790a4bf1e16a2e1fea1b975
Adriaan Mulder <adriaan.mulder@gmail.com>
2025-03-13 22:45:56 -0700

    Remove unused cookie logic

--- stories/pages/home.stories.js
+++
@@ -1,8 +0,0 @@
-import Home from "pages/indexold";
-
-export default {
-  title: "Pages/Home",
-  component: Home,
-};
-
-export const HomePage = () => <Home />
--- util/server-only/cookies.js
+++ util/server-only/cookies.js
@@ -2,24 +2,6 @@ import { getIronSession } from 'iron-session'
 import { cookies } from 'next/headers'
 import { sessionOptions } from 'util/server-only/iron-session'
 
-// Legacy API routes (Pages Router)
-export const cookieToken = async ({req, res}) => {
-  const session = await getIronSession(req, res, sessionOptions)
-  return session.token
-}
-
-export const cookieLogin = async ({req, res, token}) => {
-  const session = await getIronSession(req, res, sessionOptions)
-  session.token = token
-  await session.save()
-}
-
-export const cookieLogout = async ({req, res}) => {
-  const session = await getIronSession(req, res, sessionOptions)
-  session.destroy()
-}
-
-// App Router versions
 export const getToken = async () => {
   const session = await getIronSession(cookies(), sessionOptions)
   return session.token
