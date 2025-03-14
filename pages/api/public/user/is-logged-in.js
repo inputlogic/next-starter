5a335d1b1f4bfdae5d327485689d9b72d7f47890
Adriaan Mulder <adriaan.mulder@gmail.com>
2025-03-13 20:23:14 -0700

    Migrate api proxy to app router

--- pages/api/public/user/is-logged-in.js
+++
@@ -1,8 +0,0 @@
-import { cookieToken } from 'util/server-only/cookies'
-
-export default async (req, res) => {
-  const token = await cookieToken({req, res})
-  res.status(200).json({
-    isLoggedIn: Boolean(token)
-  })
-}
--- util/server-only/cookies.js
+++ util/server-only/cookies.js
@@ -1,6 +1,8 @@
 import { getIronSession } from 'iron-session'
+import { cookies } from 'next/headers'
 import { sessionOptions } from 'util/server-only/iron-session'
 
+// Legacy API routes (Pages Router)
 export const cookieToken = async ({req, res}) => {
   const session = await getIronSession(req, res, sessionOptions)
   return session.token
@@ -14,6 +16,23 @@ export const cookieLogin = async ({req, res, token}) => {
 
 export const cookieLogout = async ({req, res}) => {
   const session = await getIronSession(req, res, sessionOptions)
+  session.destroy()
+}
+
+// App Router versions
+export const getToken = async () => {
+  const session = await getIronSession(cookies(), sessionOptions)
+  return session.token
+}
+
+export const setToken = async (token) => {
+  const session = await getIronSession(cookies(), sessionOptions)
+  session.token = token
+  await session.save()
+}
+
+export const removeToken = async () => {
+  const session = await getIronSession(cookies(), sessionOptions)
   session.destroy()
 }
 
