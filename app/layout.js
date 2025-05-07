fb2f9280d57ac3d2826bdd2e64e846b6879fa0da
Adriaan Mulder <adriaan.mulder@gmail.com>
2025-05-07 16:38:25 -0700

    Add maintenance page and upcoming maintenance warning

--- app/layout.js
+++ app/layout.js
@@ -3,11 +3,13 @@
 import 'util/sentry'
 import 'util/settings'
 import 'styles/index.scss'
+import { Maintenance } from 'components/maintenance'
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body>
+        <Maintenance />
         {children}
       </body>
     </html>
