02d9536fd5b309607e41fc3171f63021c245d9f9
Cory Schadt <cory@inputlogic.ca>
2025-06-17 10:00:12 -0700

    Hotfix/ better import of button

--- components/form/submit-button/index.js
+++ components/form/submit-button/index.js
@@ -1,12 +1,16 @@
 import { useFormContext } from 'react-hook-form'
-import { Button } from '../../button'
+import { Button } from 'components/button'
 
 export const SubmitButton = (props) => {
-  const { formState: { isSubmitting } } = useFormContext()
-  return <Button
-    type='submit'
-    isLoading={isSubmitting}
-    disabled={isSubmitting}
-    {...props}
-  />
+  const {
+    formState: { isSubmitting },
+  } = useFormContext()
+  return (
+    <Button
+      type="submit"
+      isLoading={isSubmitting}
+      disabled={isSubmitting}
+      {...props}
+    />
+  )
 }
