80d8e4fe80eb0d7dfc80db05b937787e13d3a182
Cory Schadt <cory@inputlogic.ca>
2025-06-17 10:26:29 -0700

    Setup DatePickerSelect to use connectByRef

--- components/form/inputs/index.js
+++ components/form/inputs/index.js
@@ -11,7 +11,17 @@ export const RadioButton = connectByRef(
   ({name, error, props}) =>
     ({name, error, label: Case.capital(props.value || '')})
 )
+export const DatePickerSelect = connectByRef(
+  Inputs.DatePickerSelect,
+  // DatePicker needs special handling for value/onChange
+  ({name, error, props}) => ({
+    name,
+    error: error?.message,
+    label: Case.capital(name.split('.').slice(-1)[0]),
+    value: props.value,
+    onValueChange: props.onChange
+  })
+)
 // TODO: get these other inputs working
-// export const DatePickerSelect = connectByRef(Inputs.DatePickerSelect)
 // export const SelectInput = connectByRef(Inputs.SelectInput)
 // export const FileUpload = connectByRef(Inputs.FileUpload)
