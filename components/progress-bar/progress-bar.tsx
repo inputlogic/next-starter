447326abf6c76023005976560bd2b65fffb0d12a
Adriaan Mulder <adriaan.mulder@gmail.com>
2025-09-18 13:15:17 -0700

    Convert progress-bar to typescript

--- components/progress-bar/progress-bar.tsx
+++ components/progress-bar/progress-bar.tsx
@@ -1,8 +1,14 @@
 import { Root, Indicator } from '@radix-ui/react-progress'
+import { FC } from 'react'
 
 import styles from './progress-bar.module.scss'
 
-export const Progress = ({ progress = 0, displayText = 'Uploading' }) => {
+interface ProgressProps {
+  progress?: number
+  displayText?: string
+}
+
+export const Progress: FC<ProgressProps> = ({ progress = 0, displayText = 'Uploading' }) => {
   return (
     <Root className={styles.root} value={progress} max={100}>
       <Indicator
